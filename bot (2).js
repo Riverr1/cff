3b661d45eee3a01b3cd570aa00086b49534b04fd
Riverr1 <44487673+Riverr1@users.noreply.github.com>
2018-10-27 13:16:37 +0400

    Add files via upload

---
+++ bot (2).js
@@ -0,0 +1,10 @@
+client.on('ready', () => {
+  console.log(----------------);
+     console.log(Desert Bot- Script By : EX Clan);
+       console.log(----------------);
+     console.log(ON ${client.guilds.size} Servers '     Script By : EX Clan ');
+   console.log(----------------);
+ console.log(Logged in as ${client.user.tag}!);
+client.user.setGame(وتستمر الحياة .,"http://twitch.tv/Dream%22)
+client.user.setStatus("dnd")
+});
